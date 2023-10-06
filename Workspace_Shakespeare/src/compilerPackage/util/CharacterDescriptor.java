d51ab664f42cdd77d1623c626b365c38477133b9
Federico <41119122+fgelosa@users.noreply.github.com>
2023-10-06 10:36:11 +0200

    small changes

--- Workspace_Shakespeare/src/compilerPackage/util/CharacterDescriptor.java
+++ Workspace_Shakespeare/src/compilerPackage/util/CharacterDescriptor.java
@@ -1,14 +1,49 @@
 package compilerPackage.util;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class CharacterDescriptor {
+	
+private static Hashtable<String,String> photos;
+	
+	public static void modifyPhotoPath(String personae,String path) {
+		photos.replace(personae,path);
+	}
+	
+	public static String getPhotoPath(String personae) {
+		return photos.get(personae);
+	}
+	
+	public static void createBasePhotoPath() {
+		photos.put("Banquo", "base_Banquo.jpg");
+		photos.put("Gertrude", "base_Gertrude.jpg");
+		photos.put("Ghost", "base_Ghost.jpg");
+		photos.put("Hamlet", "base_Hamlet.jpg");
+		photos.put("Juliet", "base_Juliet.jpg");
+		photos.put("Ladymacbeth", "base_Ladymacbeth.jpg");
+		photos.put("Mercutio", "base_Mercutio.jpg");
+		photos.put("Ophelia", "base_Ophelia.jpg");
+		photos.put("Polonio", "base_Polonio.jpg");
+		photos.put("Prospero", "base_Prospero.jpg");
+		photos.put("Romeo", "base_Romeo.jpg");
+		photos.put("Shylock", "base_Shylock.jpg");
+	}
+
+	public static void resetPhotoPath(String personae) {
+		photos.replace(personae, "base_"+personae+".jpg");
+	}
+	
+	public static void resetAllPhotoPath() {
+		createBasePhotoPath();
+	}
+	
 
 	public String name;
 	public boolean onStage;
 	private int currentValue;
 	private ArrayList<Integer> memoryList;
-
+	
 	public CharacterDescriptor(String n) {
 		name = n;
 		onStage = false;
